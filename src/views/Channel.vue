2d27fc6a32b6194c91a8ab8d6964aed08b9820df
Taerande <revive0405@gmail.com>
2022-01-21 17:47:51 +0900

    updated

--- src/views/Channel.vue
+++ src/views/Channel.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="about">
-    <h1>{{this.$router}}</h1>
+<v-container>
+<v-row>
+  <v-col>
     <h2>this is channel page</h2>
+    <h1>{{$route.params}}</h1>
+  </v-col>
+</v-row>
+<v-row>
+  <div class="twitch-video">
+   <iframe src="https://clips.twitch.tv/embed?clip=HorribleBlatantSwordNerfBlueBlaster&parent=localhost:8080" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
   </div>
+</v-row>
+</v-container>
 </template>
 <script>
 export default {
-  computed: {
-    channelName() {
-      return this.$route;
-    },
+  updated() {
+    if (Object.keys(this.$route.params).length === 0) { this.$router.push({ path: '/' }); }
   },
 
 };
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,6 +5,15 @@
      :key="item.id"
      class="col-4"
      >
+     <router-link :to="{name: 'Channel',
+     query:{
+       q: item.login
+     },
+       params:{
+         name: item.display_name,
+         id: item.id,
+       }
+     }">
      <v-card>
        <v-card-title>{{item.display_name}}</v-card-title>
        <v-avatar>
@@ -14,6 +23,7 @@
        <v-card-subtitle>{{item.id}}</v-card-subtitle>
        <v-card-subtitle>조회수 : {{item.view_count}}</v-card-subtitle>
      </v-card>
+     </router-link>
 
     </v-col>
   </v-row>
@@ -22,17 +32,53 @@
   </v-row>
 </v-container>
 </template>
+
 <script>
+import axios from 'axios';
 
 export default {
   data() {
     return {
+      headerConfig: {
+        Authorization: 'Bearer ebuqjmjwopv9uroc6jc8yso7kjnokm',
+        'Client-id': 'c3ovwwcs9lhrx1rq13fsllzqfu9o9t',
+        Accept: 'application/json',
+      },
+
+      lists: [],
       userInfoLists: [],
     };
   },
   methods: {
+    searchChannel() {
+      axios.get('https://api.twitch.tv/helix/search/channels', {
+        params: {
+          query: this.$route.query.q,
+        },
+        headers: this.headerConfig,
+      }).then((res) => {
+        this.lists = [];
+        res.data.data.forEach((element) => {
+          if (element.title.length > 0 && element.game_id > 0) {
+            axios.get('https://api.twitch.tv/helix/users', {
+              params: {
+                id: element.id,
+              },
+              headers: this.headerConfig,
+            }).then((resp) => {
+              resp.data.data.forEach((ele) => {
+                this.lists.push(ele);
+              });
+              this.lists.sort((a, b) => b.view_count - a.view_count);
+            });
+          }
+        });
+        this.$store.commit('SET_SearchList', this.lists);
+      }).catch((error) => console.log(error));
+    },
   },
-  mounted() {
+  created() {
+    this.searchChannel();
   },
 };
 </script>
